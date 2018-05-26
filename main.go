6fa79038c162e5fc4b32d7173318be1afe508f30
paul.dircksen <paul.dircksen>
2018-05-26 21:39:33 +0200

    add qrcode generation

--- main.go
+++ main.go
@@ -4,21 +4,34 @@ import (
 	"encoding/json"
 	"fmt"
 
+	b64 "encoding/base64"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	qrcode "github.com/skip2/go-qrcode"
 )
 
 // Response message
 type Response struct {
 	Message string `json:"msg"`
+	Qrcode  string `json:"qr"`
 }
 
 // Handler for output
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("Received body: ", request.Body)
 
+	// create qr code
+	png, err := qrcode.Encode("https://www.smile-feedback.de/vote/1234", qrcode.Medium, 256)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	// create base64 string
+	uEnc := b64.URLEncoding.EncodeToString(png)
+
 	r := Response{
 		Message: "Hello from golang function",
+		Qrcode:  uEnc,
 	}
 	rbytes, err := json.Marshal(r)
 	if err != nil {
